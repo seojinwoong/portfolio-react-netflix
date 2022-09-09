a0a9561bf2fb729615f5a004479b17804440b811
SEOJINWOONG <tjwlsdnd12@naver.com>
2022-09-10 02:44:18 +0900

    배포관련 url수정

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -7,7 +7,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 const MovieRow = ({id, title, fetchURL, isLargePoster}) => {
 
